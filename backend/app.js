2e0002cad25312ca85b34330671197344f060249
YanchikSivova <sivova0108@gmail.com>
2026-02-20 15:32:33 +0300

    app without styles

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-// const cors = require("cors");
+const cors = require("cors");
 
 const logger = require("./middleware/logger");
 const productsRouter = require("./routes/products");
@@ -7,7 +7,9 @@ const productsRouter = require("./routes/products");
 const app  = express();
 const PORT = process.env.PORT || 3000
 
-// app.use(cors());
+app.use(cors({
+    origin: "http://localhost:3001"
+}));
 
 app.use(express.json());
 
--- backend/data/products.js
+++ backend/data/products.js
@@ -1,5 +1,5 @@
 module.exports=[
     {id: 1, title: "Кофе", price:199, description: "Очень горький кофе"},
     {id: 2, title: "Чай", price:149, description: "Очень ароматный чай"},
-    {id: 3, title: "Шоколад", price:129, description: "Очень сладкий кофе"}
+    {id: 3, title: "Шоколад", price:129, description: "Очень сладкий шоколад"}
 ];
