d1c102df2f0475a85693724e17e55701fafe231b
YanchikSivova <sivova0108@gmail.com>
2026-02-20 15:36:13 +0300

    docs and frontend

---
+++ frontend/src/api/productsApi.js
@@ -0,0 +1,27 @@
+import { api } from "./apiClient";
+
+/**
+ * TODO (Практика 4):
+ * Реализуйте функции работы с API.
+ * Подсказка: используйте api.get/post/patch/delete и возвращайте data.
+ */
+
+export async function getProducts() {
+    const response = await api.get("/products");
+    return response.data;
+}
+
+export async function createProduct(payload) {
+    const response = await api.post("/products", payload);
+    return response.data;
+}
+
+export async function updateProduct(id, patch) {
+  const response = await api.patch(`/products/${id}`, patch);
+  return response.data;
+}
+
+export async function deleteProduct(id) {
+  const response = await api.delete(`/products/${id}`);
+  return response.data;
+}
